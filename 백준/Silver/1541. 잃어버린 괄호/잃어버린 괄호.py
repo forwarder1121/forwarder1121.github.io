0afd1ab26f3cfb70f65e89d2a6e91b3bd9818391
김동환 <66872094+forwarder1121@users.noreply.github.com>
2025-03-20 17:54:58 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 104 ms, Memory: 37004 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
@@ -1,20 +1,24 @@
 import re
+#input
 
-expr = input()
-tokens = re.findall(r'\d+|[+\-*/]', expr)
+input=re.findall(r"\d+|[+-]",input())
+result=0
+minusAppear=False
 
-result=int(tokens[0])
-stack=[]
-stack_mode=False
-for idx in range(1,len(tokens)):
-    if idx%2==1: # 부호
-        if tokens[idx]=='-':
-            stack_mode=True
-    if idx%2==0: # 숫자
-        if stack_mode==False:
-            result+=int(tokens[idx])
-        if stack_mode==True:
-            stack.append(int(tokens[idx]))
-for item in stack:
-    result-=item
+for i in range(len(input)):
+    # check signal
+    if(input[i]=="-"):
+        minusAppear=True
+        continue
+    elif (input[i]=="+"):
+        continue
+    # logic (number)
+    if(minusAppear==False):
+        result+=int(input[i])
+    else:
+        result-=int(input[i])
+
 print(result)
+
+#time complexity : O(N)
+#55-50+40
