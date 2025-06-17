01da2390bc4a808f1c2ff7f465a328ec5afaf40a
김동환 <66872094+forwarder1121@users.noreply.github.com>
2025-06-17 10:42:15 +0900

    [Silver IV] Title: 동전 0, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.py
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -3,11 +3,10 @@ input=sys.stdin.readline
 
 N,K=map(int,input().split())
 coins=[int(input()) for _ in range(N)]
-result=0
-
+total=0
 for coin in reversed(coins):
     if K>=coin:
-        result+=K//coin
+        total+=K//coin
         K%=coin
-    
-print(result)
+
+print(total)
