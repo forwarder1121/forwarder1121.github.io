e3058c106b9f96112f778da5c4bd9fbcbc6cd347
김동환 <66872094+forwarder1121@users.noreply.github.com>
2025-06-22 01:14:00 +0900

    [Silver I] Title: 단지번호붙이기, Time: 36 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,35 +1,28 @@
 import sys
-from collections import deque
 input=sys.stdin.readline
 
 N=int(input())
 graph=[list(map(int,input().strip())) for _ in range(N)]
 visited=[[False]*N for _ in range(N)]
-dx=[0,0,-1,1]
-dy=[1,-1,0,0]
-results=[]
-def bfs(x,y):
-    area=1
-    queue=deque()
-    queue.append((x,y))
-    visited[x][y]=True
-    while queue:
-        cx,cy=queue.popleft()
-        for i in range(4):
-            nx=cx+dx[i]
-            ny=cy+dy[i]
-            if 0<=nx<N and 0<=ny<N:
-                if not visited[nx][ny] and graph[nx][ny]==1:
-                    queue.append((nx,ny))
-                    visited[nx][ny]=True
-                    area+=1
+dx=[-1,1,0,0]
+dy=[0,0,-1,1]
 
+def dfs(x,y):
+    area=1
+    visited[x][y]=True
+    for i in range(4):
+        nx=x+dx[i]
+        ny=y+dy[i]
+        if 0<=nx<N and 0<=ny<N:
+            if not visited[nx][ny] and graph[nx][ny]==1:
+                area+=dfs(nx,ny)
     return area
 
+results=[]
 for i in range(N):
     for j in range(N):
         if not visited[i][j] and graph[i][j]==1:
-            results.append(bfs(i,j))
+            results.append(dfs(i,j))
 
 results.sort()
 print(len(results))
