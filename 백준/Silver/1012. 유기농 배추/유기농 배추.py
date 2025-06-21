c63aa8375e5124b47e2921cc402b1d1d403a6508
김동환 <66872094+forwarder1121@users.noreply.github.com>
2025-06-21 13:26:50 +0900

    [Silver II] Title: 유기농 배추, Time: 44 ms, Memory: 32776 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,40 +1,32 @@
 import sys
-from collections import deque
+sys.setrecursionlimit(10**6)
 input=sys.stdin.readline
+
 T=int(input())
-
 dx=[-1,1,0,0]
-dy=[0,0,-1,1]
-
-def bfs(x,y):
-    queue=deque()
-    queue.append((x,y))
-    visited[x][y]=True
-    
-    while queue:
-        cx,cy=queue.popleft()
-        for i in range(4):
-            nx=cx+dx[i]
-            ny=cy+dy[i]
-            if 0<=nx<N and 0<=ny<M:
-                if not visited[nx][ny] and graph[nx][ny]==1:
-                    queue.append((nx,ny))
-                    visited[nx][ny]=True
-                
+dy=[0,0,1,-1]
+def dfs(x,y):
+    visited[y][x]=True
+    for i in range(4):
+        nx=x+dx[i]
+        ny=y+dy[i]
+        if 0<=nx<M and 0<=ny<N:
+            if not visited[ny][nx] and farm[ny][nx]==1: 
+                dfs(nx,ny)
 
 for _ in range(T):
-    N,M,K=map(int,input().split())
-    graph=[[0]*M for _ in range(N)]
+    M,N,K=map(int,input().split())
+    farm=[[0]*M for _ in range(N)]
     visited=[[False]*M for _ in range(N)]
+
     for _ in range(K):
         x,y=map(int,input().split())
-        graph[x][y]=1
+        farm[y][x]=1
     
     count=0
     for i in range(N):
         for j in range(M):
-            if not visited[i][j] and graph[i][j]==1:
-                bfs(i,j)
+            if not visited[i][j] and farm[i][j]==1:
+                dfs(j,i)
                 count+=1
     print(count)
-
