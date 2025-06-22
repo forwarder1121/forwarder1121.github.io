f1c1a00ee9ee9c18cdb0737b7d401a67bf50fc39
김동환 <66872094+forwarder1121@users.noreply.github.com>
2025-06-22 17:18:38 +0900

    [Silver I] Title: 영역 구하기, Time: 44 ms, Memory: 35064 KB -BaekjoonHub

--- 백준/Silver/2583. 영역 구하기/영역 구하기.py
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.py
@@ -1,44 +1,33 @@
 import sys
-from collections import deque 
 input=sys.stdin.readline
-
+sys.setrecursionlimit(10**6)
+M,N,K=map(int,input().split())
+graph=[[0]*N for _ in range(M)]
+visited=[[0]*N for _ in range(M)]
 dx=[-1,1,0,0]
 dy=[0,0,-1,1]
-results=[]
-
-N,M,K=map(int,input().split())
-graph=[[True]*M for _ in range(N)]
-visited=[[False]*M for _ in range(N)]
+def dfs(x,y):
+    area=1
+    visited[x][y]=True
+    for i in range(4):
+        nx=x+dx[i]
+        ny=y+dy[i]
+        if 0<=nx<M and 0<=ny<N:
+            if not visited[nx][ny] and graph[nx][ny]==0:
+                area+=dfs(nx,ny)
+    return area
 
 for _ in range(K):
     y1,x1,y2,x2=map(int,input().split())
-    for i in range(x1,x2):
-        for j in range(y1,y2):
-            graph[i][j]=False
+    for x in range(x1,x2):
+        for y in range(y1,y2):
+            graph[x][y]=1
 
-
-def bfs(x,y):
-    queue=deque()
-    queue.append((x,y))
-    visited[x][y]=True
-    area=1
-    while queue:
-        cx,cy=queue.popleft()
-        for i in range(4):
-            nx=cx+dx[i]
-            ny=cy+dy[i]
-            if 0<=nx<N and 0<=ny<M:
-                if not visited[nx][ny] and graph[nx][ny]==True:
-                    queue.append((nx,ny))
-                    visited[nx][ny]=True
-                    area+=1
-    return area
-
-for i in range(N):
-    for j in range(M):
-        if not visited[i][j] and graph[i][j]==True:
-            results.append(bfs(i,j))
-            
+results=[]
+for i in range(M):
+    for j in range(N):
+        if not visited[i][j] and graph[i][j]==0:
+            results.append(dfs(i,j))
 
 results.sort()
 print(len(results))
