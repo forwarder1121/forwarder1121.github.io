da87e4cc4f575c479a719949692e38eebb4f28c2
김동환 <66872094+forwarder1121@users.noreply.github.com>
2025-07-07 14:15:52 +0900

    [Gold III] Title: 같이 눈사람 만들래?, Time: 376 ms, Memory: 58476 KB -BaekjoonHub

--- 백준/Gold/20366. 같이 눈사람 만들래？/같이 눈사람 만들래？.py
+++ 백준/Gold/20366. 같이 눈사람 만들래？/같이 눈사람 만들래？.py
@@ -4,25 +4,18 @@ input=sys.stdin.readline
 N=int(input())
 snows=list(map(int,input().split()))
 
-# O(N^2)
 results=[]
 for i in range(len(snows)):
-    for j in range(i+1,len(snows)):
+    for j in range(i+1, len(snows)):
         results.append((snows[i]+snows[j],i,j))
-
-# sort => O(NlogN)
 results.sort()
-#print(results)
-# O(N)
+
 import math
 min_diff=math.inf
-
 for i in range(len(results)-1):
-    s1,i1,j1=results[i]
-    s2,i2,j2=results[i+1]
-    if i1!=i2 and i1!=j2 and j1!=i2 and j1!=j2:
+    s1,l1,h1=results[i]
+    s2,l2,h2=results[i+1]
+    if l1!=l2 and l1!=h2 and h1!=l2 and h1!=h2:
         min_diff=min(min_diff,s2-s1)
-    
-print(min_diff)
 
-# total time complexity : O(N^2)
+print(min_diff)
