97a37eebd22faca4298e6d958c6d3ef1edf5168a
김동환 <66872094+forwarder1121@users.noreply.github.com>
2025-07-09 08:00:18 +0900

    [Silver I] Title: RGB거리, Time: 36 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.py
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -2,19 +2,20 @@ import sys
 input=sys.stdin.readline
 
 N=int(input())
-prices=[list(map(int,input().split())) for _ in range(N)]
-dp=[[0]*3 for _ in range(N)]
+prices=[[0]*3]+[list(map(int,input().split())) for _ in range(N)]
+
+dp=[[0]*3 for _ in range(N+1)]
 
 # init
-dp[0][0]=prices[0][0]
-dp[0][1]=prices[0][1]
-dp[0][2]=prices[0][2]
+dp[1][0]=prices[1][0]
+dp[1][1]=prices[1][1]
+dp[1][2]=prices[1][2]
 
-# recursion
-for i in range(N):
+for i in range(2,N+1):
     dp[i][0]=min(dp[i-1][1],dp[i-1][2])+prices[i][0]
     dp[i][1]=min(dp[i-1][0],dp[i-1][2])+prices[i][1]
     dp[i][2]=min(dp[i-1][0],dp[i-1][1])+prices[i][2]
 
-result=min(dp[N-1])
-print(result)
+
+
+print(min(dp[N]))
