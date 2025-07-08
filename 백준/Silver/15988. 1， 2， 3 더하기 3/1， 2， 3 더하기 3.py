e639672ff34bb404bf8f05cb665dfa9989b184c1
김동환 <66872094+forwarder1121@users.noreply.github.com>
2025-07-09 08:32:04 +0900

    [Silver II] Title: 1, 2, 3 더하기 3, Time: 456 ms, Memory: 71968 KB -BaekjoonHub

--- 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.py
+++ 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.py
@@ -3,16 +3,15 @@ input=sys.stdin.readline
 
 T=int(input())
 cases=[int(input()) for _ in range(T)]
+
 MAX=max(cases)
 
 dp=[0]*(MAX+1)
 
-# init
 dp[1]=1
 dp[2]=2
 dp[3]=4
 
-# recursion
 for x in range(4,MAX+1):
     dp[x]=(dp[x-1]+dp[x-2]+dp[x-3])%1000000009
 
