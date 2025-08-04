7c04a57e54c11b6dec2af5cda9259e9be6d58579
김동환 <66872094+forwarder1121@users.noreply.github.com>
2025-08-04 15:51:04 +0900

    [Silver IV] Title: 스택, Time: 68 ms, Memory: 35896 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.py
@@ -0,0 +1,21 @@
+import sys
+from collections import deque
+input=sys.stdin.readline
+
+N=int(input())
+commands=[input().split() for _ in range(N)]
+stack=deque()
+for command in commands:
+    if command[0]=="push":
+        stack.append(command[1])
+    elif command[0]=="pop":
+        print(stack.pop() if stack else -1)
+    elif command[0]=="size":
+        print(len(stack))
+    elif command[0]=="empty":
+        print(0 if stack else 1)
+    elif command[0]=="top":
+        print(stack[-1] if stack else -1)
+
+        
+    
