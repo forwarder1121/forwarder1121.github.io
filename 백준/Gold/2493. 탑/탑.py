8ba24c06e1f89770600c9a661b68ff26d86e2254
김동환 <66872094+forwarder1121@users.noreply.github.com>
2025-08-12 21:23:35 +0900

    [Gold V] Title: 탑, Time: 564 ms, Memory: 121816 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.py
+++ 백준/Gold/2493. 탑/탑.py
@@ -1,21 +1,20 @@
 import sys
-input=sys.stdin.readline
-
 N=int(input())
 towers=list(map(int,input().split()))
-stack=[] # (index,height)
-result=[]
 
+stack=[] # (height, 1-based-index)
+result=[]
 for i in range(N):
     curr_tower=towers[i]
-    while stack and stack[-1][1]<curr_tower:
+
+    while stack and stack[-1][0]<curr_tower:
         stack.pop()
     
     if stack:
-        result.append(stack[-1][0]+1)
+        result.append(stack[-1][1])
     else:
         result.append(0)
     
-    stack.append((i,towers[i]))
+    stack.append((curr_tower,i+1))
 
 print(*result)
