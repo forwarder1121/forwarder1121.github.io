daad15fa6e5f2b5f7c0d3abd1b51f8815ba2ebb7
김동환 <66872094+forwarder1121@users.noreply.github.com>
2025-08-19 17:44:37 +0900

    [Silver I] Title: 미로 탐색, Time: 64 ms, Memory: 34968 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,27 +1,27 @@
 import sys
 from collections import deque
 input=sys.stdin.readline
-
 N,M=map(int,input().split())
 graph=[list(map(int,input().strip())) for _ in range(N)]
-visited=[[False]*M for _ in range(N)]
+dist=[[0]*M for _ in range(N)]
+
+
 dx=[-1,1,0,0]
 dy=[0,0,-1,1]
+
 def bfs(x,y):
     queue=deque()
     queue.append((x,y))
-    visited[x][y]=True
+    dist[x][y]=1
     while queue:
         cx,cy=queue.popleft()
         for i in range(4):
             nx=cx+dx[i]
             ny=cy+dy[i]
             if 0<=nx<N and 0<=ny<M:
-                if not visited[nx][ny] and graph[nx][ny]==1:
+                if graph[nx][ny]==1 and dist[nx][ny]==0:
+                    dist[nx][ny]=dist[cx][cy]+1
                     queue.append((nx,ny))
-                    visited[nx][ny]=True
-                    graph[nx][ny]=graph[cx][cy]+1
-                    
 
 bfs(0,0)
-print(graph[N-1][M-1])
+print(dist[N-1][M-1])
