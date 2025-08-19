a24230399f66935c9e1e03d662d7014b066ab23c
김동환 <66872094+forwarder1121@users.noreply.github.com>
2025-08-19 17:29:50 +0900

    [Silver I] Title: 그림, Time: 288 ms, Memory: 36404 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.py
+++ 백준/Silver/1926. 그림/그림.py
@@ -1,8 +1,10 @@
+import sys
 from collections import deque
-
+input=sys.stdin.readline
 N,M=map(int,input().split())
-graph=[list(map(int,input().split())) for _ in range(N)]
+paper=[list(map(int,input().split())) for _ in range(N)]
 visited=[[False]*M for _ in range(N)]
+
 
 dx=[-1,1,0,0]
 dy=[0,0,-1,1]
@@ -18,7 +20,7 @@ def bfs(x,y):
             nx=cx+dx[i]
             ny=cy+dy[i]
             if 0<=nx<N and 0<=ny<M:
-                if not visited[nx][ny] and graph[nx][ny]==1:
+                if not visited[nx][ny] and paper[nx][ny]==1:
                     visited[nx][ny]=True
                     queue.append((nx,ny))
                     area+=1
@@ -26,12 +28,12 @@ def bfs(x,y):
 
 count=0
 max_area=0
-
-for i in range(N):
-    for j in range(M):
-        if not visited[i][j] and graph[i][j]==1:
+for x in range(N):
+    for y in range(M):
+        if not visited[x][y] and paper[x][y]==1:
+            max_area=max(max_area,bfs(x,y))
             count+=1
-            max_area=max(max_area,bfs(i,j))
+
 
 print(count)
 print(max_area)
