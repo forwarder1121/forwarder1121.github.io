164a315eb824ba456e086f88b351c875fac966e6
김동환 <66872094+forwarder1121@users.noreply.github.com>
2025-09-08 12:37:57 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 5372 ms, Memory: 280236 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
@@ -4,31 +4,38 @@ input=sys.stdin.readline
 
 N,M=map(int,input().split())
 graph=[list(map(int,input().strip())) for _ in range(N)]
-dist=[[[0]*2 for _ in range(M)] for _ in range(N)]
-# dist[x][y][0] : unbroke wall
-# dist[x][y][1] : broke wall
+
+dist=[[[-1]*2 for _ in range(M)] for _ in range(N)]
+visited=[[[False]*2 for _ in range(M)] for _ in range(N)]
 
 dx=[-1,1,0,0]
 dy=[0,0,-1,1]
 
-dist[0][0][0]=1
+def bfs():
+    queue=deque()
+    queue.append((0,0,0))
+    dist[0][0][0]=1
+    visited[0][0][0]=True
+    while queue:
+        cx,cy,cb=queue.popleft()
+        if cx==N-1 and cy==M-1:
+            return dist[cx][cy][cb]
+        for i in range(4):
+            nx=cx+dx[i]
+            ny=cy+dy[i]
+            if 0<=nx<N and 0<=ny<M:
+                # case 1 : road
+                if graph[nx][ny]==0 and not visited[nx][ny][cb]:
+                    dist[nx][ny][cb]=dist[cx][cy][cb]+1
+                    queue.append((nx,ny,cb))
+                    visited[nx][ny][cb]=True
+                # case 2 : wall -> break if available
+                elif graph[nx][ny]==1 and cb==0 and not visited[nx][ny][cb]:
+                    dist[nx][ny][1]=dist[cx][cy][cb]+1
+                    queue.append((nx,ny,1))
+                    visited[nx][ny][cb]=1
+    return -1       
 
-queue=deque()
-queue.append((0,0,0))
+print(bfs())
 
-while queue:
-    cx,cy,broken=queue.popleft()
-    if cx==N-1 and cy==M-1:
-        print(dist[cx][cy][broken])
-        sys.exit()
-    for i in range(4):
-        nx=cx+dx[i]
-        ny=cy+dy[i]
-        if 0<=nx<N and 0<=ny<M:
-            if graph[nx][ny]==0 and dist[nx][ny][broken]==0:
-                dist[nx][ny][broken]=dist[cx][cy][broken]+1
-                queue.append((nx,ny,broken))
-            elif graph[nx][ny]==1 and broken==0 and dist[nx][ny][broken]==0:
-                dist[nx][ny][1]=dist[cx][cy][broken]+1
-                queue.append((nx,ny,1))
-print(-1)    
+# 3d - bfs
