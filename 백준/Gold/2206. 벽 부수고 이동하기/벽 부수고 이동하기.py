4c431994d9178da76b436e25f0159c8d66a2cb91
김동환 <66872094+forwarder1121@users.noreply.github.com>
2025-09-15 12:40:44 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 4020 ms, Memory: 207192 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
@@ -2,40 +2,44 @@ import sys
 from collections import deque
 input=sys.stdin.readline
 
-N,M=map(int,input().split())
-graph=[list(map(int,input().strip())) for _ in range(N)]
-
-dist=[[[-1]*2 for _ in range(M)] for _ in range(N)]
-visited=[[[False]*2 for _ in range(M)] for _ in range(N)]
-
 dx=[-1,1,0,0]
 dy=[0,0,-1,1]
+
+# bfs - 3d
+
+N,M=map(int,input().split())
+graph=[list(map(int,input().strip())) for _ in range(N)]
+dist = [[[ -1 for _ in range(2) ] for _ in range(M)] for _ in range(N)]
+
+
 
 def bfs():
     queue=deque()
     queue.append((0,0,0))
     dist[0][0][0]=1
-    visited[0][0][0]=True
     while queue:
         cx,cy,cb=queue.popleft()
+
+        # base case
         if cx==N-1 and cy==M-1:
             return dist[cx][cy][cb]
+
         for i in range(4):
             nx=cx+dx[i]
             ny=cy+dy[i]
             if 0<=nx<N and 0<=ny<M:
-                # case 1 : road
-                if graph[nx][ny]==0 and not visited[nx][ny][cb]:
+                # next is 0
+                if dist[nx][ny][cb]==-1 and graph[nx][ny]==0:
                     dist[nx][ny][cb]=dist[cx][cy][cb]+1
                     queue.append((nx,ny,cb))
-                    visited[nx][ny][cb]=True
-                # case 2 : wall -> break if available
-                elif graph[nx][ny]==1 and cb==0 and not visited[nx][ny][cb]:
+                # next is 1 and broken is 0 -> break
+                elif dist[nx][ny][1]==-1 and graph[nx][ny]==1 and cb==0:
                     dist[nx][ny][1]=dist[cx][cy][cb]+1
                     queue.append((nx,ny,1))
-                    visited[nx][ny][cb]=1
-    return -1       
+    return -1
 
 print(bfs())
 
-# 3d - bfs
+                
+                
+        
