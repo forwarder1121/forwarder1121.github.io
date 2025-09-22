9416a7f83411d97434e9a64c07d18e32dafe2b26
김동환 <66872094+forwarder1121@users.noreply.github.com>
2025-09-22 22:26:21 +0900

    [Gold III] Title: 다리 만들기, Time: 108 ms, Memory: 37604 KB -BaekjoonHub

--- 백준/Gold/2146. 다리 만들기/다리 만들기.py
+++ 백준/Gold/2146. 다리 만들기/다리 만들기.py
@@ -3,61 +3,68 @@ import math
 from collections import deque
 input=sys.stdin.readline
 
-dx=[-1,1,0,0]
-dy=[0,0,-1,1]
-
 # init
 N=int(input())
 graph=[list(map(int,input().split())) for _ in range(N)]
-marking=[[-1]*N for _ in range(N)]
+distance=[[[-1]*2 for _ in range(N)] for _ in range(N)] # (distance, nation_num)
 
-# marking
-partion_number=0
+dx=[-1,1,0,0]
+dy=[0,0,-1,1]
+
+# logic flow
+# 1. marking nation number
+# 2. multi-source bfs
+# 2-1. init bfs queue
+# 2-2. bfs
+
+# 1. marking nation number
+nation_number=0
 for x in range(N):
     for y in range(N):
-        if graph[x][y]==1 and marking[x][y]==-1:
-            partion_number+=1
+        if graph[x][y]==1 and distance[x][y][0]==-1:
+            nation_number+=1
             queue=deque()
             queue.append((x,y))
-            marking[x][y]=partion_number
+            distance[x][y][0]=0
+            distance[x][y][1]=nation_number
             while queue:
                 cx,cy=queue.popleft()
                 for i in range(4):
                     nx=cx+dx[i]
                     ny=cy+dy[i]
                     if 0<=nx<N and 0<=ny<N:
-                        if graph[nx][ny]==1 and marking[nx][ny]==-1:
-                            marking[nx][ny]=partion_number
+                        if graph[nx][ny]==1 and distance[nx][ny][0]==-1:
                             queue.append((nx,ny))
-            
+                            distance[nx][ny][0]=0
+                            distance[nx][ny][1]=nation_number
 
-# multi-source bfs
-queue=deque()
-distance=[[-1]*N for _ in range(N)]
+# 2. multi-source bfs
+# 2-1. init bfs queue
+queue=deque() # (x, y, nation_number)
 for x in range(N):
     for y in range(N):
-        if graph[x][y]==1:
+        if distance[x][y][0]==0:
             for i in range(4):
                 nx=x+dx[i]
                 ny=y+dy[i]
                 if 0<=nx<N and 0<=ny<N:
-                    if graph[nx][ny]==0 and distance[nx][ny]==-1:
-                        queue.append((x,y))
-                        distance[x][y]=0
+                    if graph[nx][ny]==0:
+                        queue.append((x,y,distance[x][y][1]))
+                        break
 
-min_length=math.inf
+# 2-2. bfs
+min_length=math.inf               
 while queue:
-    cx,cy=queue.popleft()
-    cm=marking[cx][cy]
+    cx,cy,cn=queue.popleft()
     for i in range(4):
         nx=cx+dx[i]
         ny=cy+dy[i]
         if 0<=nx<N and 0<=ny<N:
-            if not distance[nx][ny]==-1 and not cm==marking[nx][ny]:
-                min_length=min(min_length,distance[nx][ny]+distance[cx][cy])
-            if graph[nx][ny]==0 and distance[nx][ny]==-1:
-                distance[nx][ny]=distance[cx][cy]+1
-                marking[nx][ny]=cm
-                queue.append((nx,ny))
+            if not distance[nx][ny][0]==-1 and not distance[nx][ny][1]==cn:
+                min_length=min(min_length,distance[nx][ny][0]+distance[cx][cy][0])
+            if distance[nx][ny][0]==-1:
+                queue.append((nx,ny,cn))
+                distance[nx][ny][0]=distance[cx][cy][0]+1
+                distance[nx][ny][1]=cn
 
 print(min_length)
