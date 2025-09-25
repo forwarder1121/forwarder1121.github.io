1394d3a6d26fa998be001c2672ce26c5ddc9cff0
김동환 <66872094+forwarder1121@users.noreply.github.com>
2025-09-26 01:25:29 +0900

    [Gold IV] Title: 빙산, Time: 4144 ms, Memory: 35052 KB -BaekjoonHub

--- 백준/Gold/2573. 빙산/빙산.py
+++ 백준/Gold/2573. 빙산/빙산.py
@@ -7,31 +7,39 @@ N,M=map(int,input().split())
 graph=[list(map(int,input().split())) for _ in range(N)]
 
 dx=[-1,1,0,0]
-dy=[0,0,-1,1]
+dy=[0,0,1,-1]
 
-# functions
-def melt() -> bool:
-    isMelted=False
+# logic flow 
+# 1. simulate melting
+# 2. check connected component using bfs/dfs
+
+def melt()->bool:
     diff=[[0]*M for _ in range(N)]
-    for cx in range(N):
-        for cy in range(M):
-            if graph[cx][cy]>0:
-                for i in range(4):
-                    nx=cx+dx[i]
-                    ny=cy+dy[i]
-                    if 0<=nx<N and 0<=ny<M:
-                        if graph[nx][ny]==0:
-                            diff[cx][cy]+=1
-                            isMelted=True
     for x in range(N):
         for y in range(M):
-            graph[x][y]=max(0,graph[x][y]-diff[x][y])
+            if graph[x][y]>0:
+                for i in range(4):
+                    nx=x+dx[i]
+                    ny=y+dy[i]
+                    if 0<=nx<N and 0<=ny<M:
+                        if graph[nx][ny]==0:
+                            diff[x][y]+=1
+                            
+    isMelted=False
+    for x in range(N):
+        for y in range(M):
+            if graph[x][y] > 0: 
+                new_h = max(0, graph[x][y] - diff[x][y])
+                if new_h < graph[x][y]:
+                    isMelted = True
+                graph[x][y] = new_h    
     return isMelted
 
-def get_connected_components()->int:
+def get_connected_component()->int:
     visited=[[False]*M for _ in range(N)]
+    cp=0
 
-    def bfs(x,y):
+    def bfs(x,y)->None:
         queue=deque()
         queue.append((x,y))
         visited[x][y]=True
@@ -41,25 +49,21 @@ def get_connected_components()->int:
                 nx=cx+dx[i]
                 ny=cy+dy[i]
                 if 0<=nx<N and 0<=ny<M:
-                    if graph[nx][ny]>0 and not visited[nx][ny]:
+                    if not visited[nx][ny] and graph[nx][ny]>0:
                         queue.append((nx,ny))
                         visited[nx][ny]=True
+        
 
-    cp=0
     for x in range(N):
         for y in range(M):
-            if graph[x][y]>0 and not visited[x][y]:
+            if not visited[x][y] and graph[x][y]>0:
                 bfs(x,y)
                 cp+=1
     return cp
 
-
-
-# main logic
-day=0
+day=0                
 while True:
-    connected_components=get_connected_components()
-    if connected_components>=2:
+    if get_connected_component()>=2:
         print(day)
         break
     if not melt():
