101a6f8db25d5b29eeb1cd4baa56828017404292
김동환 <66872094+forwarder1121@users.noreply.github.com>
2025-10-06 02:57:31 +0900

    [Platinum V] Title: 백조의 호수, Time: 4072 ms, Memory: 136580 KB -BaekjoonHub

--- 백준/Platinum/3197. 백조의 호수/백조의 호수.py
+++ 백준/Platinum/3197. 백조의 호수/백조의 호수.py
@@ -2,63 +2,62 @@ import sys
 from collections import deque
 input=sys.stdin.readline
 
-# init 
+# input
 R,C=map(int,input().split())
 graph=[list(input().strip()) for _ in range(R)]
-distance=[[-1]*C for _ in range(R)]
+visited=[[False]*C for _ in range(R)]
 
+# init
 dx=[-1,1,0,0]
 dy=[0,0,-1,1]
 
-# initial settings
-water_q=deque()
-swan_q=deque()
-next_swan_q=deque()
+water_queue=deque()
+swan_queue=deque()
+next_swan_queue=deque()
 
 swans=[]
 for x in range(R):
     for y in range(C):
-        if graph[x][y]!="X":
-            water_q.append((x,y))
         if graph[x][y]=="L":
             swans.append((x,y))
+        if graph[x][y] in ("L","."):
+            water_queue.append((x,y))
 
 (sx,sy),(tx,ty)=swans
-swan_q.append((sx,sy))
-
-visited=[[False]*C for _ in range(R)]
+swan_queue.append((sx,sy))
 visited[sx][sy]=True
 
-# frontier BFS
+# functions
+# layered multi-source BFS
+def melt()->None:
+    for _ in range(len(water_queue)):
+        cx,cy=water_queue.popleft()
+        for i in range(4):
+            nx=cx+dx[i]
+            ny=cy+dy[i]
+            if 0<=nx<R and 0<=ny<C:
+                if graph[nx][ny]=="X":
+                    graph[nx][ny]="."
+                    water_queue.append((nx,ny))
+
+# frontier BFS    
 def canMeet()->bool:
-    global swan_q, next_swan_q
-    while swan_q:
-        x,y=swan_q.popleft()
-        if (x,y)==(tx,ty):
+    while swan_queue:
+        cx,cy=swan_queue.popleft()
+        if (cx,cy)==(tx,ty):
             return True
         for i in range(4):
-            nx=x+dx[i]
-            ny=y+dy[i]
+            nx=cx+dx[i]
+            ny=cy+dy[i]
             if 0<=nx<R and 0<=ny<C:
                 if not visited[nx][ny]:
                     visited[nx][ny]=True
                     if graph[nx][ny] in (".","L"):
-                        swan_q.append((nx,ny))
+                        swan_queue.append((nx,ny))
                     elif graph[nx][ny]=="X":
-                        next_swan_q.append((nx,ny))
-    return False
+                        next_swan_queue.append((nx,ny))
 
-# multi-source BFS
-def melt():
-    for _ in range((len(water_q))):
-        x,y=water_q.popleft()
-        for i in range(4):
-            nx=x+dx[i]
-            ny=y+dy[i]
-            if 0<=nx<R and 0<=ny<C:
-                if graph[nx][ny]=="X":
-                    graph[nx][ny]="."
-                    water_q.append((nx,ny))
+    return False
 
 # simulation
 day=0
@@ -67,7 +66,5 @@ while True:
         print(day)
         break
     melt()
-    swan_q,next_swan_q=next_swan_q,deque()
+    swan_queue,next_swan_queue=next_swan_queue,deque()
     day+=1
-
-    
