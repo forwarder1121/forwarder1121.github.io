9dfb934d44755f212c0899e58cfb3e3749f8814a
김동환 <66872094+forwarder1121@users.noreply.github.com>
2025-10-27 00:57:41 +0900

    [Gold IV] Title: 최단경로, Time: 524 ms, Memory: 71644 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.py
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -1,14 +1,13 @@
-import sys,heapq,math
+import sys,math,heapq
 input=sys.stdin.readline
 
 # input
 V,E=map(int,input().split())
 start=int(input())
 graph=[[] for _ in range(V+1)]
-
 for _ in range(E):
-    u,v,cost=map(int,input().split())
-    graph[u].append((v,cost))
+    u,v,w=map(int,input().split())
+    graph[u].append((v,w))
 
 def dijkstra(start,graph,N):
     dist=[math.inf]*(N+1)
@@ -22,9 +21,9 @@ def dijkstra(start,graph,N):
 
         if cur_cost!=dist[cur_node]:
             continue
-
-        for next_node,next_cost in graph[cur_node]:
-            new_cost=cur_cost+next_cost
+        
+        for next_node,cost in graph[cur_node]:
+            new_cost=dist[cur_node]+cost
             if new_cost<dist[next_node]:
                 dist[next_node]=new_cost
                 heapq.heappush(pq,(new_cost,next_node))
@@ -32,6 +31,7 @@ def dijkstra(start,graph,N):
     return dist
 
 results=dijkstra(start,graph,V)
+
 for i in range(1,V+1):
     if results[i]==math.inf:
         print("INF")
