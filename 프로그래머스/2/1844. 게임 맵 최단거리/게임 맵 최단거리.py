eefea0dde228e4d0cf73f8a7cb22555884c8d254
김동환 <66872094+forwarder1121@users.noreply.github.com>
2025-11-10 15:10:43 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 18.38 ms, Memory: 9.29 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,30 +1,30 @@
 from collections import deque
-
-dx=[-1,1,0,0]
-dy=[0,0,-1,1]
-
+# BFS
 def solution(graph):
+    dx=[-1,1,0,0]
+    dy=[0,0,-1,1]
     N=len(graph)
     M=len(graph[0])
+    visited=[[False]*M for _ in range(N)]
     dist=[[0]*M for _ in range(N)]
     
+    def bfs(x,y):
+        queue=deque()
+        queue.append((x,y))
+        dist[x][y]=1
+        while queue:
+            cx,cy=queue.popleft() # bfs
+            if cx==N-1 and cy==M-1:
+                return dist[N-1][M-1]
+            for i in range(4):
+                nx=cx+dx[i]
+                ny=cy+dy[i]
+                if 0<=nx<N and 0<=ny<M:
+                    if graph[nx][ny]==1 and not visited[nx][ny]:
+                        visited[nx][ny]=True
+                        dist[nx][ny]=dist[cx][cy]+1
+                        queue.append((nx,ny))
+                        
+        return -1
     
-    queue=deque()
-    queue.append((0,0))
-    dist[0][0]=1
-    
-    while queue:
-        cx,cy=queue.popleft()
-        if cx==N-1 and cy==M-1:
-            return dist[N-1][M-1]
-        for i in range(4):
-            nx=cx+dx[i]
-            ny=cy+dy[i]
-            if 0<=nx<N and 0<=ny<M:
-                if graph[nx][ny]==1 and dist[nx][ny]==0:
-                    dist[nx][ny]=dist[cx][cy]+1
-                    queue.append((nx,ny))
-        
-    
-    
-    return -1 
+    return bfs(0,0)
