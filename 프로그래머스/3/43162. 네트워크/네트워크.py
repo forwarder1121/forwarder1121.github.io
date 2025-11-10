777462df4e4cb60fb960bff0f2392d783228e781
김동환 <66872094+forwarder1121@users.noreply.github.com>
2025-11-10 13:06:53 +0900

    [level 3] Title: 네트워크, Time: 0.52 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,16 +1,15 @@
-def solution(n, computers):
-    visited=[False]*n
-    answer=0
-    
+# connected-component
+def solution(N, computers):
+    visited=[False]*N # 0-based
+    count=0
     def dfs(u):
-        visited[u]=True
-        for v in range(n):
+        for v in range(N):
             if computers[u][v]==1 and not visited[v]:
+                visited[v]=True
                 dfs(v)
-    
-    for i in range(n):
-        if not visited[i]:
+                
+    for i in range(N):
+        if visited[i]==False:
             dfs(i)
-            answer+=1
-            
-    return answer
+            count+=1
+    return count
