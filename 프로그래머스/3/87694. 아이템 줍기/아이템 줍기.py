adcf5c5506e777bc3f56ed127e2fdd5740e90141
김동환 <66872094+forwarder1121@users.noreply.github.com>
2025-11-12 16:43:45 +0900

    [level 3] Title: 아이템 줍기, Time: 2.48 ms, Memory: 9.31 MB -BaekjoonHub

--- 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.py
+++ 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.py
@@ -1,43 +1,39 @@
 from collections import deque
-def solution(rectangle, characterX, characterY, itemX, itemY):
+SIZE=51
+SCALE=2
+dx=[-1,1,0,0]
+dy=[0,0,-1,1]
+def solution(rectangles, characterX, characterY, itemX, itemY):
     
-    SCALE=2
-    rects=[]
-    for x1,y1,x2,y2 in rectangle:
-        rects.append((x1*SCALE,y1*SCALE,x2*SCALE,y2*SCALE))
-    
-    board=[[False]*101 for _ in range(101)]
-    for x1,y1,x2,y2 in rects:
+    # making graph
+    graph=[[False]*SIZE*SCALE for _ in range(SIZE*SCALE)]
+    for x1,y1,x2,y2 in rectangles:
+        x1,y1,x2,y2=x1*SCALE,y1*SCALE,x2*SCALE,y2*SCALE
         for x in range(x1,x2+1):
             for y in range(y1,y2+1):
-                board[x][y]=True
-    
-    for x1,y1,x2,y2 in rects:
+                graph[x][y]=True
+    for x1,y1,x2,y2 in rectangles:
+        x1,y1,x2,y2=x1*SCALE,y1*SCALE,x2*SCALE,y2*SCALE
         for x in range(x1+1,x2):
             for y in range(y1+1,y2):
-                board[x][y]=False
-    
-    dist=[[-1]*101 for _ in range(101)]
-    
-    dx=[-1,1,0,0]
-    dy=[0,0,-1,1]
-    
+                graph[x][y]=False
+        
+                
+    # BFS
+    dist=[[-1]*SIZE*SCALE for _ in range(SIZE*SCALE)]
     queue=deque()
     queue.append((characterX*SCALE,characterY*SCALE))
     dist[characterX*SCALE][characterY*SCALE]=0
     while queue:
         cx,cy=queue.popleft()
-        if cx==itemX*2 and cy==itemY*2:
-            return dist[cx][cy]//SCALE
+        if cx==itemX*SCALE and cy==itemY*SCALE:
+            return dist[cx][cy]//2
         for i in range(4):
             nx=cx+dx[i]
             ny=cy+dy[i]
-            if 0<=nx<101 and 0<=ny<101:
-                if board[nx][ny]==True and dist[nx][ny]==-1:
+            if 0<=nx<SIZE*SCALE and 0<=ny<SIZE*SCALE:
+                if graph[nx][ny] and dist[nx][ny]==-1:
                     dist[nx][ny]=dist[cx][cy]+1
                     queue.append((nx,ny))
-                    
-        
-    
-    answer = 0
-    return answer
+
+    return -1
