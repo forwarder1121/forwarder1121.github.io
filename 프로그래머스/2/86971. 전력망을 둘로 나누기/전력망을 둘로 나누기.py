a1ebc4d3464e99a7a2aa40bf76d1ed7a3babc2b6
김동환 <66872094+forwarder1121@users.noreply.github.com>
2025-12-02 03:10:50 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 5.04 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -2,41 +2,31 @@ from collections import defaultdict
 import math
 def solution(N, edges):
     
-    # init graph
+    # making graph
     graph=defaultdict(list)
     for u,v in edges:
         graph[u].append(v)
         graph[v].append(u)
     
-    def dfs(deleted_edge,node_index,visited)->int:
-        count=1
-        visited[node_index]=True
+    # return connected-component
+    def dfs(cur_node,deleted_edge,visited)->int:
         u,v=deleted_edge
-        for next_node in graph[node_index]:
-            if (u,v)==(node_index,next_node) or (v,u)==(node_index,next_node):
+        visited[cur_node]=True
+        count=1
+        for next_node in graph[cur_node]:
+            if (cur_node,next_node)==(u,v) or (next_node,cur_node)==(v,u):
                 continue
             if not visited[next_node]:
-                count+=dfs(deleted_edge,next_node,visited)
-
+                count+=dfs(next_node,deleted_edge,visited)
         return count
-    
+                
     answer=math.inf
-    
     for deleted_edge in edges:
-        # cut edge
-        # connected component
         visited=[False]*(N+1) # 1-based
-        a=dfs(deleted_edge,1,visited)
-        b=N-a
-        diff=abs(a-b)
+        size_a=dfs(deleted_edge[0],deleted_edge,visited)
+        size_b=N-size_a
+        diff=abs(size_a-size_b)
         answer=min(answer,diff)
         
+    
     return answer
-
-
-        
-    
-    
-    
-    
-    
