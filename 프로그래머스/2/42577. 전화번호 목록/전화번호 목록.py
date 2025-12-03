8f7a70e7124d7be1716f5c08edd2bc656d281fcc
김동환 <66872094+forwarder1121@users.noreply.github.com>
2025-12-03 17:10:30 +0900

    [level 2] Title: 전화번호 목록, Time: 124.82 ms, Memory: 29.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -0,0 +1,10 @@
+def solution(phone_book):
+    phone_book.sort()
+    
+    print(help(str.startswith))
+    
+    for p1,p2 in zip(phone_book,phone_book[1:]):
+        if p2.startswith(p1):
+            return False
+        
+    return True
