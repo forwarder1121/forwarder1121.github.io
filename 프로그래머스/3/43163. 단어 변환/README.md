ab52106574346dc626238f60f1bca246128cdab2
김동환 <66872094+forwarder1121@users.noreply.github.com>
2025-12-21 20:13:31 +0900

    [level 3] Title: 단어 변환, Time: 2.68 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/README.md
+++ 프로그래머스/3/43163. 단어 변환/README.md
@@ -1,10 +1,10 @@
 # [level 3] 단어 변환 - 43163 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43163?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43163?language=python3) 
 
 ### 성능 요약
 
-메모리: 86.7 MB, 시간: 0.40 ms
+메모리: 9.27 MB, 시간: 2.68 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 11월 11일 00:20:25
+2025년 12월 21일 20:13:26
 
 ### 문제 설명
 
--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -1,28 +1,40 @@
-import math
+from collections import defaultdict,deque
 def solution(begin, target, words):
-    visited=[False]*(len(words))
-    def one_diff(a,b)->bool:
+    
+    def one_diff(w1,w2):
         count=0
-        for i in range(len(a)):
-            if a[i]!=b[i]:
+        for i in range(len(w1)):
+            if w1[i]!=w2[i]:
                 count+=1
         return count==1
     
-    def dfs(state,depth)->int:
-        # base-condition
-        if state==target:
-            return depth
-        # apply
-        best=math.inf
-        for i in range(len(words)):
-            if one_diff(words[i],state) and not visited[i]:
-                visited[i]=True
-                cand=dfs(words[i],depth+1)
-                if cand<best:
-                    best=cand
-                visited[i]=False
-        return best
-        
-    answer=dfs(begin,0)
+    # INIT GRAPH
+    N=len(words)
+    graph=defaultdict(list)
+    for u in range(N):
+        for v in range(N):
+            if one_diff(words[u],words[v]):
+                graph[u].append(v)
+         
+    # BFS (Multi-Source)
+    visited=[False]*N
+    distance=[0]*N
+    queue=deque()
+    for i in range(N):
+        if one_diff(begin,words[i]):
+            queue.append(i)
+            visited[i]=True
+            distance[i]=1
     
-    return 0 if answer==math.inf else answer
+    while queue:
+        cur_node=queue.popleft()
+        # base-condition
+        if words[cur_node]==target:
+            return(distance[cur_node])
+        for next_node in graph[cur_node]:
+            if not visited[next_node]:
+                visited[next_node]=True
+                distance[next_node]=distance[cur_node]+1
+                queue.append(next_node)
+                
+    return 0
