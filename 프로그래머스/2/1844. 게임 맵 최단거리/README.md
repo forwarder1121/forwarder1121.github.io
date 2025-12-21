e6475af20db8b6e10f182c554d0758a58a3906ac
김동환 <66872094+forwarder1121@users.noreply.github.com>
2025-12-21 19:45:19 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 11.83 ms, Memory: 9.42 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=python3) 
 
 ### 성능 요약
 
-메모리: 54.6 MB, 시간: 8.31 ms
+메모리: 9.42 MB, 시간: 11.83 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 11월 10일 15:56:15
+2025년 12월 21일 19:45:16
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,30 +1,32 @@
 from collections import deque
-# BFS
+dx=[-1,1,0,0]
+dy=[0,0,-1,1]
+
 def solution(graph):
-    dx=[-1,1,0,0]
-    dy=[0,0,-1,1]
+    
+    # INIT
     N=len(graph)
     M=len(graph[0])
     visited=[[False]*M for _ in range(N)]
-    dist=[[0]*M for _ in range(N)]
+    distance=[[-1]*M for _ in range(N)]
     
-    def bfs(x,y):
-        queue=deque()
-        queue.append((x,y))
-        dist[x][y]=1
-        while queue:
-            cx,cy=queue.popleft() # bfs
-            if cx==N-1 and cy==M-1:
-                return dist[N-1][M-1]
-            for i in range(4):
-                nx=cx+dx[i]
-                ny=cy+dy[i]
-                if 0<=nx<N and 0<=ny<M:
-                    if graph[nx][ny]==1 and not visited[nx][ny]:
-                        visited[nx][ny]=True
-                        dist[nx][ny]=dist[cx][cy]+1
-                        queue.append((nx,ny))
-                        
-        return -1
     
-    return bfs(0,0)
+    # BFS
+    queue=deque()
+    queue.append((0,0))
+    visited[0][0]=True
+    distance[0][0]=1
+    while queue:
+        cx,cy=queue.popleft()
+        # base-condition
+        if cx==N-1 and cy==M-1:
+            return distance[cx][cy]
+        for i in range(4):
+            nx=cx+dx[i]
+            ny=cy+dy[i]
+            if 0<=nx<N and 0<=ny<M:
+                if not visited[nx][ny] and graph[nx][ny]==1:
+                    visited[nx][ny]=True
+                    distance[nx][ny]=distance[cx][cy]+1
+                    queue.append((nx,ny))
+    return -1
