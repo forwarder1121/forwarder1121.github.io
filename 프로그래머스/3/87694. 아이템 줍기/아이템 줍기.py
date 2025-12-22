fa7416ef3e95b0a3b373a080e80983e60c9b88e0
김동환 <66872094+forwarder1121@users.noreply.github.com>
2025-12-22 16:34:18 +0900

    [level 3] Title: 아이템 줍기, Time: 2.23 ms, Memory: 9.46 MB -BaekjoonHub

--- 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.py
+++ 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.py
@@ -1,42 +1,52 @@
-SCALE=2
-SIZE=50
-MAX=SCALE*SIZE+1
 from collections import deque
+SIZE=51 # 1-based
+SCALE=2
+dx=[-1,1,0,0]
+dy=[0,0,-1,1]
 def solution(rectangles, characterX, characterY, itemX, itemY):
     
-    # Making Graph
-    graph=[[False]*MAX for _ in range(MAX)]
-    dist=[[-1]*MAX for _ in range(MAX)]
-    for x1,y1,x2,y2 in rectangles:
-        sx1,sy1,sx2,sy2 = x1*SCALE,y1*SCALE,x2*SCALE,y2*SCALE
-        for x in range(sx1,sx2+1):
-            for y in range(sy1,sy2+1):
+    # Transformation by scaling 2
+    rectangle=[[x1*2, y1*2, x2*2, y2*2] for x1,y1,x2,y2 in rectangles]
+    characterX=characterX*2
+    characterY=characterY*2
+    itemX=itemX*2
+    itemY=itemY*2
+    
+    
+    # INIT GRAPH 
+    graph=[[False]*SIZE*SCALE for _ in range(SIZE*SCALE)]
+    
+    # PAINTING
+    for x1,y1,x2,y2 in rectangle:
+        for x in range(x1,x2+1):
+            for y in range(y1,y2+1):
                 graph[x][y]=True
     
-    for x1,y1,x2,y2 in rectangles:
-        sx1,sy1,sx2,sy2 = x1*SCALE,y1*SCALE,x2*SCALE,y2*SCALE
-        for x in range(sx1+1,sx2):
-            for y in range(sy1+1,sy2):
+    # FILLING
+    for x1,y1,x2,y2 in rectangle:
+        for x in range(x1+1,x2):
+            for y in range(y1+1,y2):
                 graph[x][y]=False
-    
-    dx=[-1,1,0,0]
-    dy=[0,0,-1,1]
     
     # BFS
     queue=deque()
-    queue.append((characterX*SCALE,characterY*SCALE))
-    dist[characterX*SCALE][characterY*SCALE]=0
-    
+    visited=[[False]*SIZE*SCALE for _ in range(SIZE*SCALE)]
+    distance=[[-1]*SIZE*SCALE for _ in range(SIZE*SCALE)]
+    queue.append((characterX,characterY))
+    visited[characterX][characterY]=True
+    distance[characterX][characterY]=0
     while queue:
         cx,cy=queue.popleft()
-        if cx==itemX*SCALE and cy==itemY*SCALE:
-            return dist[cx][cy]//2
+        visited
         for i in range(4):
             nx=cx+dx[i]
             ny=cy+dy[i]
-            if 0<=nx<MAX and 0<=ny<MAX:
-                if graph[nx][ny] and dist[nx][ny]==-1:
-                    dist[nx][ny]=dist[cx][cy]+1
+            if 0<=nx<SIZE*SCALE and 0<=ny<SIZE*SCALE:
+                if not visited[nx][ny] and graph[nx][ny]:
+                    distance[nx][ny]=distance[cx][cy]+1
+                    visited[nx][ny]=True
                     queue.append((nx,ny))
+                    
+        
     
-    return -1
+    return distance[itemX][itemY]//2
