cd0a3022ccf40c2294e93adc669bb5b7baed9576
김동환 <66872094+forwarder1121@users.noreply.github.com>
2026-01-03 00:25:21 +0900

    [Gold IV] Title: N-Queen, Time: 29280 ms, Memory: 33608 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.py
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -1,42 +1,44 @@
 import sys
+from copy import deepcopy
 input=sys.stdin.readline
 
 N=int(input())
+board=[[0]*N for _ in range(N)]
+used=[False]*N
+answer=0
 
+diag_1=[False]*(2*N+1) # sum
+diag_2=[False]*(2*N+1) # diff
 
-used_cols=[False]*N
-used_diag1=[False]*(2*N-1) # row + col
-used_diag2=[False]*(2*N-1) # row - col + (N-1)
-
-def can_place(row,col)->bool:
-    if used_cols[col]:
+def can_place(row,col):
+    if used[col]:
         return False
-    elif used_diag1[row+col]:
+    if diag_1[row+col]:
         return False
-    elif used_diag2[row-col+N-1]:
+    if diag_2[row-col+N]:
         return False
-    else:
-        return True
+    return True
 
 
-def back_tracking(row):
+def dfs(row):
     # base-condition
     if row==N:
         return 1
-    count=0
+    
+    result=0
     for col in range(N):
-        # pruning
-        if not can_place(row,col):
-            continue
-        # apply
-        used_cols[col]=True
-        used_diag1[row+col]=True
-        used_diag2[row-col+N-1]=True
-        count+=back_tracking(row+1)
-        # undo
-        used_cols[col]=False
-        used_diag1[row+col]=False
-        used_diag2[row-col+N-1]=False
-    return count
+        if can_place(row,col):
+            # apply
+            diag_1[row+col]=True
+            diag_2[row-col+N]=True
+            used[col]=True
+            # dfs
+            result+=dfs(row+1)
+            # undo
+            diag_1[row+col]=False
+            diag_2[row-col+N]=False
+            used[col]=False
+    
+    return result
 
-print(back_tracking(0))
+print(dfs(0))            
