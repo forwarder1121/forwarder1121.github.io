711c826c53b3c785d1ca2eb678a09308d6729cfc
김동환 <66872094+forwarder1121@users.noreply.github.com>
2026-01-03 00:53:21 +0900

    [Gold III] Title: 감시, Time: 2176 ms, Memory: 35692 KB -BaekjoonHub

--- 백준/Gold/15683. 감시/감시.py
+++ 백준/Gold/15683. 감시/감시.py
@@ -1,21 +1,15 @@
 import sys,math
-from itertools import permutations
 from copy import deepcopy
 input=sys.stdin.readline
 
+# INPUT
 N,M=map(int,input().split())
 board=[list(map(int,input().split())) for _ in range(N)]
 
-cctvs=[]
-for x in range(N):
-    for y in range(M):
-        if 1<=board[x][y]<=5:
-            cctvs.append((x,y,board[x][y]))
+dx=[1,0,-1,0]
+dy=[0,1,0,-1]
 
-dx=[-1,0,1,0]
-dy=[0,-1,0,1]
-
-directions={
+type2directions={
     1:[[0],[1],[2],[3]],
     2:[[0,2],[1,3]],
     3:[[0,1],[1,2],[2,3],[3,0]],
@@ -23,36 +17,39 @@ directions={
     5:[[0,1,2,3]]
 }
 
-
-def count_blind(board):
-    return sum(row.count(0) for row in board)
-
-def watch(board,x,y,direction):
-    for d in direction:
-        nx,ny=x,y
-        while True:
-            nx+=dx[d]
-            ny+=dy[d]
-            if not (0<=nx<N and 0<=ny<M):
-                break
-            if board[nx][ny]==6:
-                break
-            if board[nx][ny]==0:
-                board[nx][ny]=-1
+cctvs=[]
+for x in range(N):
+    for y in range(M):
+        if 1<=board[x][y]<=5:
+            cctvs.append((x,y,board[x][y]))
 
 def dfs(level,board):
     # base-condition
     if level==len(cctvs):
-        return count_blind(board)
-
-    x,y,ctype=cctvs[level]
-    best=math.inf
-
-    for direction in directions[ctype]:
-        newboard=deepcopy(board)
-        watch(newboard,x,y,direction)
-        best=min(best,dfs(level+1,newboard))
+        return sum(row.count(0) for row in board)
     
-    return best
+    best=math.inf
+    # apply
+    x,y,ctype=cctvs[level]
+    for directions in type2directions[ctype]:
+        new_board=deepcopy(board)
+        for d in directions:
+            nx=x
+            ny=y
+            while True:
+                nx+=dx[d]
+                ny+=dy[d]
+                if not (0<=nx<N and 0<=ny<M):
+                    break
+                if new_board[nx][ny]==6:
+                    break
+                if new_board[nx][ny]==0:
+                    new_board[nx][ny]=-1
 
-print(dfs(0,board))
+        # dfs
+        result=dfs(level+1,new_board)
+        best=min(best,result)
+
+        # undo -> replaced by deepcopy
+    return best
+print(dfs(0,board)),
