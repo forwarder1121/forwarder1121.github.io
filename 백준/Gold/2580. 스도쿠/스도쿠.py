2e0da3831bb7a56af0a8c77b8d0cb719409bb16b
김동환 <66872094+forwarder1121@users.noreply.github.com>
2026-01-04 20:59:58 +0900

    [Gold IV] Title: 스도쿠, Time: 1736 ms, Memory: 33564 KB -BaekjoonHub

---
+++ 백준/Gold/2580. 스도쿠/스도쿠.py
@@ -0,0 +1,49 @@
+import sys
+from copy import deepcopy
+input=sys.stdin.readline
+
+# INPUT
+board=[list(map(int,input().split())) for _ in range(9)]
+
+row_check = [[False] * 10 for _ in range(9)]
+col_check = [[False] * 10 for _ in range(9)]
+box_check = [[False] * 10 for _ in range(9)]
+
+vacancies=[]
+for x in range(9):
+    for y in range(9):
+        if board[x][y]==0:
+            vacancies.append((x,y))
+        else:
+            row_check[x][board[x][y]]=True
+            col_check[y][board[x][y]]=True
+            box_check[(x//3)*3+(y//3)][board[x][y]]=True
+
+def dfs(idx):
+    # base-condition
+    if idx==len(vacancies):
+        return True
+    
+    px,py=vacancies[idx]
+    box_idx=(px//3)*3+(py//3)
+    for num in range(1,10):
+        if not row_check[px][num] and not col_check[py][num] and not box_check[box_idx][num]:
+            # apply
+            board[px][py]=num
+            row_check[px][num]=True
+            col_check[py][num]=True
+            box_check[box_idx][num]=True
+            # bfs
+            if dfs(idx+1):
+                return True
+            # undo
+            board[px][py]=0
+            row_check[px][num]=False
+            col_check[py][num]=False
+            box_check[box_idx][num]=False
+            
+    return False
+
+dfs(0)
+for row in range(9):
+    print(*board[row])
