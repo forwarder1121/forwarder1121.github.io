b4cee986df4a98ae58bcab3d9c1982f8d50fde38
김동환 <66872094+forwarder1121@users.noreply.github.com>
2026-01-06 02:07:18 +0900

    [Gold V] Title: 계란으로 계란치기, Time: 3680 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.py
+++ 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.py
@@ -4,16 +4,10 @@ input=sys.stdin.readline
 N=int(input())
 eggs=[list(map(int,input().split())) for _ in range(N)]
 
-
-answer=0
 def dfs(idx):
-    
-    global answer
     # base-condition
     if idx==N:        
-        broken=len([egg for egg in eggs if egg[0]<=0])
-        answer=max(answer,broken)
-        return
+        return len([egg for egg in eggs if egg[0]<=0])
     
     alive_idx=[]
     for i in range(N):
@@ -23,10 +17,9 @@ def dfs(idx):
             alive_idx.append(i)         
 
     if not alive_idx or eggs[idx][0]<=0:
-        dfs(idx+1)
-        return
+        return dfs(idx+1)
 
-
+    answer=0
     for aidx in alive_idx:
         # do
         before1=eggs[idx][0]
@@ -34,12 +27,12 @@ def dfs(idx):
         eggs[idx][0]-=eggs[aidx][1]
         eggs[aidx][0]-=eggs[idx][1]
         # dfs
-        dfs(idx+1)
+        answer=max(answer,dfs(idx+1))
         # undo
         eggs[idx][0]=before1
         eggs[aidx][0]=before2
 
-    return  
+    return answer
             
-dfs(0)
-print(answer)
+
+print(dfs(0))
