1ab4721c27acd1913bdf901f586df0421ce41756
김동환 <66872094+forwarder1121@users.noreply.github.com>
2026-01-07 17:56:12 +0900

    [Gold III] Title: 사다리 조작, Time: 1720 ms, Memory: 112240 KB -BaekjoonHub

--- 백준/Gold/15684. 사다리 조작/사다리 조작.py
+++ 백준/Gold/15684. 사다리 조작/사다리 조작.py
@@ -1,67 +1,63 @@
 import sys
+input = sys.stdin.readline
 
-# 전역 변수 접근 속도를 높이기 위해 함수 내부로 로직을 넣습니다.
-def solve():
-    input = sys.stdin.readline
-    N, M, H = map(int, input().split())
-    
-    board = [[False] * (N + 1) for _ in range(H + 1)]
-    for _ in range(M):
-        a, b = map(int, input().split())
-        board[a][b] = True
+N, M, H = map(int, input().split())
 
-    # 가로선이 놓인 상태를 체크하는 함수
-    def is_possible():
-        for i in range(1, N + 1):
-            pos = i
-            for j in range(1, H + 1):
-                if board[j][pos]:
-                    pos += 1
-                elif board[j][pos - 1]:
-                    pos -= 1
-            if pos != i:
-                return False
-        return True
+# board[r][c] : r번째 높이에서 c와 c+1 연결 여부
+board = [[False]*N for _ in range(H)]
 
-    def dfs(count, r, c, limit):
-        # [가지치기] 현재 홀수 개인 세로선 사이 구간을 계산
-        # 이 로직을 넣으면 탐색량이 획기적으로 줄어듭니다.
-        if count == limit:
-            return is_possible()
+for _ in range(M):
+    a, b = map(int, input().split())
+    board[a-1][b-1] = True
 
-        for i in range(r, H + 1):
-            start_c = c if i == r else 1
-            for j in range(start_c, N):
-                if not board[i][j] and not board[i][j-1] and not board[i][j+1]:
-                    board[i][j] = True
-                    if dfs(count + 1, i, j + 2, limit):
-                        return True
-                    board[i][j] = False
-        return False
+INF = 4  # 문제 조건상 3 초과는 불가능 처리
 
-    # [핵심 최적화] 홀수 개 가로선 구간 체크
-    # i번과 i+1번 세로선 사이의 가로선 개수가 홀수면, 
-    # 결국 마지막엔 최소 1개의 가로선이 더 필요하다는 뜻입니다.
-    odd_count = 0
-    for j in range(1, N):
-        count = 0
-        for i in range(1, H + 1):
-            if board[i][j]:
-                count += 1
-        if count % 2 != 0:
-            odd_count += 1
+def simulate():
+    # 모든 세로선 i가 i로 내려오는지 확인
+    for i in range(N):
+        col = i
+        for r in range(H):
+            if board[r][col]:
+                col += 1
+            elif col > 0 and board[r][col-1]:
+                col -= 1
+        if col != i:
+            return False
+    return True
 
-    # 필요한 최소 가로선 개수가 3개보다 많으면 바로 종료
-    if odd_count > 3:
-        print(-1)
-        return
+def P(cnt, start):
+    # 이미 3개 초과면 불가능
+    if cnt > 3:
+        return INF
 
-    # 0개부터 시작
-    for limit in range(odd_count, 4): # odd_count부터 시작하는 것도 팁
-        if dfs(0, 1, 1, limit):
-            print(limit)
-            return
-            
-    print(-1)
+    # 현재 상태가 정답이면 더 안 놓아도 됨
+    if simulate():
+        return cnt
 
-solve()
+    # 3개까지 써봤는데도 안 되면 불가능
+    if cnt == 3:
+        return INF
+
+    res = INF
+
+    for idx in range(start, H*(N-1)):
+        r = idx // (N-1)
+        c = idx % (N-1)
+
+        # 놓을 수 없는 경우
+        if board[r][c]:
+            continue
+        if c > 0 and board[r][c-1]:
+            continue
+        if c < N-2 and board[r][c+1]:
+            continue
+
+        # 선택
+        board[r][c] = True
+        res = min(res, P(cnt + 1, idx + 1))
+        board[r][c] = False
+
+    return res
+
+ans = P(0, 0)
+print(ans if ans <= 3 else -1)
