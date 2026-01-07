897ab4506f18f0f03ae7e00b5147f28226e40c18
김동환 <66872094+forwarder1121@users.noreply.github.com>
2026-01-07 14:36:59 +0900

    [Gold III] Title: 감시, Time: 948 ms, Memory: 126992 KB -BaekjoonHub

--- 백준/Gold/15683. 감시/감시.py
+++ 백준/Gold/15683. 감시/감시.py
@@ -1,55 +1,47 @@
-import sys,math
+import sys
 from copy import deepcopy
 input=sys.stdin.readline
 
-# INPUT
 N,M=map(int,input().split())
 board=[list(map(int,input().split())) for _ in range(N)]
-
-dx=[1,0,-1,0]
-dy=[0,1,0,-1]
-
-type2directions={
-    1:[[0],[1],[2],[3]],
-    2:[[0,2],[1,3]],
-    3:[[0,1],[1,2],[2,3],[3,0]],
-    4:[[0,1,2],[1,2,3],[2,3,0],[3,0,1]],
-    5:[[0,1,2,3]]
-}
 
 cctvs=[]
 for x in range(N):
     for y in range(M):
         if 1<=board[x][y]<=5:
-            cctvs.append((x,y,board[x][y]))
+            cctvs.append((board[x][y],x,y))
 
-def dfs(level,board):
-    # base-condition
-    if level==len(cctvs):
-        return sum(row.count(0) for row in board)
-    
-    best=math.inf
-    # apply
-    x,y,ctype=cctvs[level]
-    for directions in type2directions[ctype]:
+dx=[-1,0,1,0]
+dy=[0,-1,0,1]
+
+cctv2dirs={
+    1:[[0],[1],[2],[3]],
+    2:[[0,2],[1,3]],
+    3:[[0,1],[1,2],[2,3],[3,0]],
+    4:[[0,1,2],[1,2,3],[2,3,0],[3,0,1]],
+    5:[[0,1,2,3]],
+}
+
+def P(idx,board):
+    if idx==len(cctvs):
+        return sum(row.count(1)+row.count(0) for row in board)
+    answer=65
+    ctype,cx,cy=cctvs[idx]
+    for dirs in cctv2dirs[ctype]:
         new_board=deepcopy(board)
-        for d in directions:
-            nx=x
-            ny=y
+        new_board[cx][cy]=-1
+        for dir in dirs:
+            nx,ny=cx,cy
             while True:
-                nx+=dx[d]
-                ny+=dy[d]
                 if not (0<=nx<N and 0<=ny<M):
                     break
                 if new_board[nx][ny]==6:
                     break
-                if new_board[nx][ny]==0:
-                    new_board[nx][ny]=-1
+                new_board[nx][ny]=-1
+                nx+=dx[dir]
+                ny+=dy[dir]
+                
+        answer=min(answer,P(idx+1,new_board))
+    return answer
 
-        # dfs
-        result=dfs(level+1,new_board)
-        best=min(best,result)
-
-        # undo -> replaced by deepcopy
-    return best
-print(dfs(0,board)),
+print(P(0,board))
