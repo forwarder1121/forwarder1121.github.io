a058863627ba16bc0771cd90d5bd48c9275e55a5
김동환 <66872094+forwarder1121@users.noreply.github.com>
2026-01-08 19:13:34 +0900

    [Bronze I] Title: 단어 뒤집기, Time: 120 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.py
+++ 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.py
@@ -5,11 +5,7 @@ N=int(input())
 
 for _ in range(N):
     words=input().split()
-    answer=""
+    answer=[]
     for word in words:
-        new_word=[]
-        for i in range(len(word)-1,-1,-1):
-            new_word+=word[i]
-        answer+="".join(new_word)
-        answer+=" "
-    print(answer)
+        answer.append("".join(word[::-1]))
+    print(" ".join(answer))
