d404607ee2efcc64ff5b88e090d8792abb9bfa85
김동환 <66872094+forwarder1121@users.noreply.github.com>
2026-01-09 15:22:48 +0900

    [Silver IV] Title: 덱, Time: 64 ms, Memory: 34952 KB -BaekjoonHub

--- 백준/Silver/10866. 덱/덱.py
+++ 백준/Silver/10866. 덱/덱.py
@@ -3,23 +3,40 @@ from collections import deque
 input=sys.stdin.readline
 
 N=int(input())
-commands=[input().split() for _ in range(N)]
-
-dq=deque()
-for cmd in commands:
-    if cmd[0]=="push_front":
-        dq.appendleft(cmd[1])
-    elif cmd[0]=="push_back":
-        dq.append(cmd[1])
+queue=deque()
+for _ in range(N):
+    cmd=input().split()
+    if cmd[0]=="push_back":
+        queue.append(cmd[1])
+    elif cmd[0]=="push_front":
+        queue.appendleft(cmd[1])
     elif cmd[0]=="pop_front":
-        print(dq.popleft() if dq else -1)
+        if queue:
+            print(queue.popleft())
+        else:
+            print(-1)
     elif cmd[0]=="pop_back":
-        print(dq.pop() if dq else -1)
+        if queue:
+            print(queue.pop())
+        else:
+            print(-1)
     elif cmd[0]=="size":
-        print(len(dq))
+        print(len(queue))
     elif cmd[0]=="empty":
-        print(0 if dq else 1)
+        if queue:
+            print(0)
+        else:
+            print(1)
     elif cmd[0]=="front":
-        print(dq[0] if dq else -1)
-    else:
-        print(dq[-1] if dq else -1)
+        if queue:
+            print(queue[0])
+        else:
+            print(-1)
+    elif cmd[0]=="back":
+        if queue:
+            print(queue[-1])
+        else:
+            print(-1)
+        
+        
+        
