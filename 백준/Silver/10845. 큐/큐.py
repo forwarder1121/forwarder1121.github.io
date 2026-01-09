a79212f68689c98c994960fa6f45813c29fe77b7
김동환 <66872094+forwarder1121@users.noreply.github.com>
2026-01-09 14:18:29 +0900

    [Silver IV] Title: 큐, Time: 64 ms, Memory: 34944 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.py
+++ 백준/Silver/10845. 큐/큐.py
@@ -3,21 +3,31 @@ from collections import deque
 input=sys.stdin.readline
 
 N=int(input())
-commands=[input().split() for _ in range(N)]
 queue=deque()
-
-for command in commands:
-    cmd_type=command[0]
-    if cmd_type=="push":
-        queue.append(command[1])
-    elif cmd_type=="pop":
-        print(queue.popleft() if queue else -1)
-    elif cmd_type=="size":
+for _ in range(N):
+    cmd=input().split()
+    if cmd[0]=="push":
+        queue.append(cmd[1])
+    elif cmd[0]=="pop":
+        if queue:
+            print(queue.popleft())
+        else:
+            print(-1)
+    elif cmd[0]=="size":
         print(len(queue))
-    elif cmd_type=="empty":
-        print(0 if queue else 1)
-    elif cmd_type=="front":
-        print(queue[0] if queue else -1)
-    elif cmd_type=="back":
-        print(queue[-1] if queue else -1)
-    
+    elif cmd[0]=="empty":
+        if queue:
+            print(0)
+        else:
+            print(1)
+    elif cmd[0]=="front":
+        if queue:
+            print(queue[0])
+        else:
+            print(-1)
+    elif cmd[0]=="back":
+        if queue:
+            print(queue[-1])
+        else:
+            print(-1)
+            
