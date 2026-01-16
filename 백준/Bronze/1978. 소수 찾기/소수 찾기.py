952534fa8cb62f6337946bd0fd9ca36ea7ab4355
김동환 <66872094+forwarder1121@users.noreply.github.com>
2026-01-16 15:59:29 +0900

    [Bronze II] Title: 소수 찾기, Time: 36 ms, Memory: 34536 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.py
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -1,21 +1,17 @@
 import sys,math
 input=sys.stdin.readline
 
-# INPUT
-SIZE=1001
 N=int(input())
-numbers=list(map(int,input().split()))
+queries=list(map(int,input().split()))
 
-# SIEVE
-is_prime=[True]*SIZE
-is_prime[0]=False
+is_prime=[True]*(1001) # 1-based
 is_prime[1]=False
-for i in range(2,int(math.sqrt(SIZE))+1):
+
+for i in range(2,int(math.sqrt(1001))+1):
     if is_prime[i]:
         j=2
-        while i*j<SIZE:
+        while i*j<1001:
             is_prime[i*j]=False
             j+=1
 
-answer=len([number for number in numbers if is_prime[number]])
-print(answer)
+print(len([num for num in queries if is_prime[num]]))
