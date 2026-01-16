49fb83572e2928e34505f1192f16b0b54b91c0e8
김동환 <66872094+forwarder1121@users.noreply.github.com>
2026-01-16 16:09:59 +0900

    [Silver III] Title: 소수 구하기, Time: 316 ms, Memory: 42352 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -1,23 +1,17 @@
 import sys,math
 input=sys.stdin.readline
 
-SIZE=1000001
-
-# INPUT
 M,N=map(int,input().split())
 
-# SIEVE
-is_prime=[True]*SIZE
-is_prime[0]=False
+MAX=int(1e6)
+is_prime=[True]*(MAX+1) # 1-based
 is_prime[1]=False
-for i in range(2,int(math.sqrt(SIZE))+1):
+
+for i in range(2,int(math.sqrt(MAX))+1):
     if is_prime[i]:
-        j=2
-        while i*j<SIZE:
-            is_prime[i*j]=False
-            j+=1
+        for j in range(i*i,MAX+1,i):
+            is_prime[j]=False
 
 for i in range(M,N+1):
     if is_prime[i]:
         print(i)
-        
