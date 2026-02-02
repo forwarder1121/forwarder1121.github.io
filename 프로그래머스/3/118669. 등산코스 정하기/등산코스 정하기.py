864d3ccc10d09d23160aa2a92550d3882275575c
김동환 <66872094+forwarder1121@users.noreply.github.com>
2026-02-02 20:10:46 +0900

    [level 3] Title: 등산코스 정하기, Time: 707.99 ms, Memory: 90.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/118669. 등산코스 정하기/등산코스 정하기.py
@@ -0,0 +1,45 @@
+import math,heapq
+from collections import defaultdict
+def solution(N, paths, gates, summits):
+    
+    # GRAPH : graph[u]=(v,cost)
+    graph=defaultdict(list)
+    for i,j,w in paths:
+        graph[i].append((j,w))
+        graph[j].append((i,w))
+        
+    intensity=[math.inf]*(N+1) # 1-based
+    
+    pq=[] # PQ : (cost,node)
+    
+    gates=set(gates)
+    summits=set(summits)
+    
+    # Multi-source
+    for start in gates:
+        intensity[start]=0
+        heapq.heappush(pq,(0,start))
+    
+    while pq:
+        cur_cost,cur_node=heapq.heappop(pq)
+        if cur_cost>intensity[cur_node]:
+            continue
+        if cur_node in summits:
+            continue
+        for next_node, next_cost in graph[cur_node]:
+            if next_node in gates:
+                continue
+            new_cost=max(next_cost,cur_cost)
+            if new_cost<intensity[next_node]:
+                intensity[next_node]=new_cost
+                heapq.heappush(pq,(new_cost,next_node))
+        
+    # intensity[] has intensity to index
+    min_summit=-1
+    min_intensity=math.inf
+    for end in sorted(summits):
+        if min_intensity>intensity[end]:
+            min_intensity=intensity[end]
+            min_summit=end
+    
+    return min_summit,min_intensity
