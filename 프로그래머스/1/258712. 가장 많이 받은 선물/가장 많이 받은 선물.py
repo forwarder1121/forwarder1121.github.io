251871408fa1c2530edd96dbf16b851cd889a100
김동환 <66872094+forwarder1121@users.noreply.github.com>
2026-02-07 22:58:50 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 5.81 ms, Memory: 9.81 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.py
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.py
@@ -1,27 +1,29 @@
-from collections import defaultdict
+
 def solution(friends, gifts):
     
-    records=defaultdict(dict)
-    gift_level=dict.fromkeys(friends,0)
-    next_gift=dict.fromkeys(friends,0)
+    records=dict.fromkeys(friends,0)
+    level=dict.fromkeys(friends,0)
     
     for friend in friends:
         records[friend]=dict.fromkeys(friends,0)
+    
     for gift in gifts:
         A,B=gift.split()
         records[A][B]+=1
-        gift_level[A]+=1
-        gift_level[B]-=1
+        level[A]+=1
+        level[B]-=1
     
+    next_gift=dict.fromkeys(friends,0)
     for person in friends:
         for friend in friends:
             if person==friend:
                 continue
-            if records[person][friend] or records[friend][person]:
-                if records[person][friend]>records[friend][person]: # 많이 줬다면
+            if records[person][friend]>records[friend][person]:
+                next_gift[person]+=1
+            elif records[person][friend]==records[friend][person]:
+                if level[person]>level[friend]:
                     next_gift[person]+=1
-            if (records[person][friend]==0 and records[friend][person]==0) or records[person][friend]==records[friend][person]:
-                    if gift_level[person]>gift_level[friend]:
-                        next_gift[person]+=1
-                        
-    return max(next_gift.values())
+    
+
+    answer = max(next_gift.values())
+    return answer
