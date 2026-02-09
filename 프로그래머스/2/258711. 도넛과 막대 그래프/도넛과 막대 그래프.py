ab32d0883b7cd70f23bbf4987cced04f6998718e
김동환 <66872094+forwarder1121@users.noreply.github.com>
2026-02-09 21:51:42 +0900

    [level 2] Title: 도넛과 막대 그래프, Time: 1051.86 ms, Memory: 169 MB -BaekjoonHub

--- 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.py
+++ 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.py
@@ -1,32 +1,31 @@
 from collections import defaultdict
 def solution(edges):
     
-    in_degree=defaultdict(int)
-    out_degree=defaultdict(int)
-    NEW_NODE=-1
-    max_node=-1
+    NEW_NODE=None
+    TOTAL_GRAPH=None
+    
+    outdegree=defaultdict(int)
+    indegree=defaultdict(int)
     nodes=set()
+    
     for a,b in edges:
-        out_degree[a]+=1
-        in_degree[b]+=1
+        outdegree[a]+=1
+        indegree[b]+=1
         nodes.add(a)
         nodes.add(b)
-    
+        
+    graph_type = [0,0,0] # [donut, stick, eight]
     for node in nodes:
-        if in_degree[node]==0 and out_degree[node]>=2:
+        if indegree[node]==0 and outdegree[node]>=2:
             NEW_NODE=node
-            break
+            TOTAL_GRAPH=outdegree[NEW_NODE]
+        elif indegree[node]>=2 and outdegree[node]==2:
+            graph_type[2]+=1
+        elif outdegree[node]==0:
+            graph_type[1]+=1
     
-    stick_graph=0
-    for node in nodes:
-        if out_degree[node]==0:
-            stick_graph+=1
+    graph_type[0]=TOTAL_GRAPH-graph_type[1]-graph_type[2]
     
-    eight_graph=0
-    for node in nodes:
-        if out_degree[node]==2 and in_degree[node]>=2:
-            eight_graph+=1
-    
-    donut_graph=out_degree[NEW_NODE]-stick_graph-eight_graph
-    
-    return [NEW_NODE,donut_graph,stick_graph,eight_graph]
+    print(graph_type)
+    answer=[NEW_NODE,*graph_type]
+    return answer
