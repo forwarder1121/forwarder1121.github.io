bce46d69ab2ec8b74f1640e9b66450f45be6da3b
김동환 <66872094+forwarder1121@users.noreply.github.com>
2026-02-12 18:43:59 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.14 ms, Memory: 9.38 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
@@ -1,18 +1,21 @@
 def solution(today, terms, privacies):
     
+    def to_days(date_str):
+        Y,M,D=map(int,date_str.split("."))
+        return Y*12*28+M*28+D
+    
     term_duration=dict()
     for term in terms:
         name,month=term.split()
         term_duration[name]=int(month)
     
-    today_year,today_month,today_day=map(int,today.split("."))
-    today_date=today_year*12*28+today_month*28+today_day
+    
+    today_date=to_days(today)
     
     answer = []
     for idx,privacy in enumerate(privacies):
         collected_date,term_name=privacy.split()
-        collected_year,collected_month,collected_day=map(int,collected_date.split("."))
-        expired_date=collected_year*12*28+collected_month*28+collected_day+term_duration[term_name]*28
+        expired_date=to_days(collected_date)+term_duration[term_name]*28
         if expired_date<=today_date:
             answer.append(idx+1)
         
