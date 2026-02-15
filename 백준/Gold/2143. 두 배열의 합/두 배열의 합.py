0fcf8ba3e6b5e8a24c981096cb6a2fd0a622761d
김동환 <66872094+forwarder1121@users.noreply.github.com>
2026-02-15 17:12:32 +0900

    [Gold III] Title: 두 배열의 합, Time: 488 ms, Memory: 118140 KB -BaekjoonHub

---
+++ 백준/Gold/2143. 두 배열의 합/두 배열의 합.py
@@ -0,0 +1,35 @@
+import sys
+from collections import defaultdict
+input=sys.stdin.readline
+
+T=int(input())
+N=int(input())
+A=list(map(int,input().split()))
+M=int(input())
+B=list(map(int,input().split()))
+
+freq_A=defaultdict(int)
+freq_B=defaultdict(int)
+
+prefix_A=[0]*(N+1)
+prefix_B=[0]*(M+1)
+
+for i in range(1,N+1):
+    prefix_A[i]=prefix_A[i-1]+A[i-1]
+
+for i in range(1,M+1):
+    prefix_B[i]=prefix_B[i-1]+B[i-1]
+
+
+for end in range(1,N+1):
+    for start in range(end):
+        interval_sum=prefix_A[end]-prefix_A[start]
+        freq_A[interval_sum]+=1
+
+answer=0
+for end in range(1,M+1):
+    for start in range(end):
+        interval_sum=prefix_B[end]-prefix_B[start]
+        answer+=freq_A[T-interval_sum]
+
+print(answer)
