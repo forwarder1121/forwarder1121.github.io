218d448e270de81957107d7f16cf1dfad99d2800
김동환 <66872094+forwarder1121@users.noreply.github.com>
2026-02-17 18:01:39 +0900

    [Gold V] Title: 회의실 배정, Time: 196 ms, Memory: 60360 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.py
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.py
@@ -7,10 +7,19 @@ meetings=[list(map(int,input().split())) for _ in range(N)]
 meetings.sort(key=lambda x:(x[1],x[0]))
 
 cur_time=0
+idx=0
 answer=0
-for start,end in meetings:
-    if start>=cur_time:
-        cur_time=end
-        answer+=1
+while True:
+    candidates=None
+    for i in range(idx,N):
+        if meetings[i][0]>=cur_time:
+            candidates=meetings[i]
+            idx=i+1
+            break
+    if not candidates:
+        break
+    
+    cur_time=candidates[1]
+    answer+=1
 
 print(answer)
