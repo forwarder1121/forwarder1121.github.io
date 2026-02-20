e1ef1030f983d9c3fc767d7b2e0888fe737e33c6
김동환 <66872094+forwarder1121@users.noreply.github.com>
2026-02-20 17:24:05 +0900

    [level 3] Title: 표현 가능한 이진트리, Time: 190.66 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/3/150367. 표현 가능한 이진트리/표현 가능한 이진트리.py
+++ 프로그래머스/3/150367. 표현 가능한 이진트리/표현 가능한 이진트리.py
@@ -1,31 +1,41 @@
 def solution(numbers):
     
     def is_valid(tree):
-        ''' Return 1 in success'''
-        def P(l,r)->bool:
-            ''' Return True if tree[l,r) is valid'''
+        ''' Returns 1 on success, 0 in failure'''
+        def P(l,r):
+            ''' Return bool according by number[l:r) is valid'''
             # base-condition
             if l>=r:
                 return True
             mid=(l+r-1)//2
+            
+            left_valid=P(l,mid)
+            right_valid=P(mid+1,r)
+            
+            if not left_valid or not right_valid:
+                return False
             if tree[mid]=="0":
                 if "1" in tree[l:r]:
                     return False
-                return True
-            return P(l,mid) and P(mid+1,r)
+            return True
         if P(0,len(tree)):
             return 1
-        return 0
+        else:
+            return 0
+            
+            
+        
+            
     
-    answer=[]
+    answer = []
     for number in numbers:
         b=bin(number)[2:]
         size=1
         while size<len(b):
             size=size*2+1
-            b=b.zfill(size)
+        b=b.zfill(size)
         result=is_valid(b)
         answer.append(result)
-    
+        
     
     return answer
