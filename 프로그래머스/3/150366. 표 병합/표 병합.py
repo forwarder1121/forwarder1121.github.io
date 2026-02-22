6b16bf83d48d169431993684dea325db1bd507f8
김동환 <66872094+forwarder1121@users.noreply.github.com>
2026-02-22 22:29:50 +0900

    [level 3] Title: 표 병합, Time: 63.82 ms, Memory: 9.61 MB -BaekjoonHub

--- 프로그래머스/3/150366. 표 병합/표 병합.py
+++ 프로그래머스/3/150366. 표 병합/표 병합.py
@@ -1,28 +1,22 @@
-
-
 def solution(commands):
     
     SIZE=50
     N=SIZE*SIZE
+    
+    # Invariant : Only Root have state
     parent=[i for i in range(N)]
     state=[""]*N
-
-
-    def to_index(r,c):
-        node_number=(r-1)*SIZE+(c-1)
-        return node_number
-
-
+    
     def find(node):
-        ''' Returns number of parent node'''
+        ''' Finds parent of node '''
         # base-condition
         if node==parent[node]:
             return node
         parent[node]=find(parent[node])
         return parent[node]
-
+    
     def union(a,b):
-        ''' Unions node a,b according to condition of problems '''
+        ''' Unions a,b node according to problem's condition'''
         ra=find(a)
         rb=find(b)
         if ra==rb:
@@ -32,13 +26,17 @@ def solution(commands):
         else:
             parent[rb]=ra
     
+    def to_index(r,c):
+        index=(r-1)*SIZE+(c-1)
+        return index
+    
     answer = []
-    for command in commands:
-        parts=command.split()
+    for cmd in commands:
+        parts=cmd.split()
         oper=parts[0]
         if oper=="PRINT":
-            r,c=map(int,parts[1:])
-            root=find(to_index(r,c))
+            r,c=parts[1:]
+            root=find(to_index(int(r),int(c)))
             result=state[root]
             if result:
                 answer.append(result)
@@ -47,46 +45,30 @@ def solution(commands):
         elif oper=="MERGE":
             r1,c1,r2,c2=map(int,parts[1:])
             union(to_index(r1,c1),to_index(r2,c2))
-                
-            
         elif oper=="UNMERGE":
             r,c=map(int,parts[1:])
             target_node=to_index(r,c)
             root=find(target_node)
-            original_value=state[root]
-            
-            # 1) collect members first (NO mutation)
-            group = []
-            for node in range(N):
-                if find(node) == root:
-                    group.append(node)
-
-            # 2) reset all
-            for node in group:
-                parent[node] = node
-                state[node] = ""
-
-            # 3) only target keeps the value
-            
-            for node in range(N):
-                if find(node)==root:
-                    parent[node]=node
-                    state[node]=""
-            state[target_node] = original_value
-            
+            orginal_value=state[root]
+            members=[node for node in range(N) if find(node)==root]
+            for member in members:
+                parent[member]=member
+                state[member]=""
+            state[target_node]=orginal_value
         elif oper=="UPDATE":
-            if len(parts)==4:
+            if len(parts)==4: 
                 ''' TYPE 1 : UPDATE r c value '''
                 r,c,value=parts[1:]
-                target_node=to_index(int(r),int(c))
-                root=find(target_node)
+                root=find(to_index(int(r),int(c)))
                 state[root]=value
-            else:
+            else: 
                 ''' TYPE 2 : UPDATE value1 value2 '''
                 value1,value2=parts[1:]
+                selected_nodes=[]
                 for node in range(N):
-                    if find(node)==node and state[node]==value1:
-                        state[node]=value2
+                    if parent[node]==node and state[node]==value1:
+                        selected_nodes.append(node)
+                for selected_node in selected_nodes:
+                    state[selected_node]=value2
     
     return answer
-
