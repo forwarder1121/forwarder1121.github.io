2a20a23d3b3e7e5417c01c4df86c1cd1d054436b
김동환 <66872094+forwarder1121@users.noreply.github.com>
2026-03-03 13:16:17 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 59.72 ms, Memory: 11 MB -BaekjoonHub

--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.py
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.py
@@ -1,16 +1,28 @@
+import math
+
 def solution(cap, n, deliveries, pickups):
     
-    d_remain=0
-    p_remain=0
+    answer = 0
     
-    answer=0
-    for i in range(n-1,-1,-1):
-        d_remain+=deliveries[i]
-        p_remain+=pickups[i]
+    S_i = 0  # suffix 배달 누적
+    P_i = 0  # suffix 수거 누적
+    
+    for i in range(n-1, -1, -1):
         
-        while d_remain>0 or p_remain>0:
-            d_remain-=cap
-            p_remain-=cap
-            answer+=(i+1)*2
-    
+        S_i += deliveries[i]
+        P_i += pickups[i]
+        
+        if S_i <= 0 and P_i <= 0:
+            continue
+        
+        trip_i = max(
+            math.ceil(S_i / cap),
+            math.ceil(P_i / cap)
+        )
+        
+        answer += trip_i * (i+1) * 2
+        
+        S_i -= trip_i * cap
+        P_i -= trip_i * cap
+        
     return answer
