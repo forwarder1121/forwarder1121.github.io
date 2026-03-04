436ce9ad4a859e5daeffd2a8f1a4610ddf0e1e98
김동환 <66872094+forwarder1121@users.noreply.github.com>
2026-03-04 15:53:43 +0900

    [level 3] Title: 산 모양 타일링, Time: 78.29 ms, Memory: 17.3 MB -BaekjoonHub

--- 프로그래머스/3/258705. 산 모양 타일링/산 모양 타일링.py
+++ 프로그래머스/3/258705. 산 모양 타일링/산 모양 타일링.py
@@ -1,19 +1,15 @@
-MOD=10007
 def solution(N, tops):
-    DP=[0]*(2*N+1) # DP[x] : x 삼각형을 덮는 경우의 수
+    MOD=10007
+    DP=[0]*(2*N+1)
     DP[0]=1
     if tops[0]:
         DP[1]=3
     else:
         DP[1]=2
-    for i in range(2,2*N+1):
-        if i%2==0: # 짝수 -> 아랫변
-            DP[i]=(DP[i-1]+DP[i-2])%MOD
-        else: # 홀수 -> 윗변
-            # top이 있으면
-            if tops[(i-1)//2]:
-                DP[i]=(DP[i-1]*2+DP[i-2])%MOD
-            else:
-                DP[i]=(DP[i-1]+DP[i-2])%MOD
-    
-    return DP[2*N]
+    for x in range(2,2*N+1):
+        if x%2==1 and tops[x//2]==1:
+            DP[x]=(DP[x-1]*2+DP[x-2])%MOD
+        else:
+            DP[x]=(DP[x-1]+DP[x-2])%MOD
+    answer = DP[2*N]
+    return answer
