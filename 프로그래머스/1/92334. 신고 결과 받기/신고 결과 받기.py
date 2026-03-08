8e490a102ab10776e14c8afc8d5284661dcd271f
김동환 <66872094+forwarder1121@users.noreply.github.com>
2026-03-08 21:20:58 +0900

    [level 1] Title: 신고 결과 받기, Time: 140.93 ms, Memory: 43.3 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.py
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.py
@@ -1,34 +1,23 @@
+from collections import defaultdict
 def solution(id_list, reports, k):
     
-    users=dict()
-    for _id in id_list:
-        users[_id]={
-            "reported_cnt":0,
-            "reporting_users":set()
-        }
-    
-    
+    edges=defaultdict(set)
+    indegree=defaultdict(int)
     for report in reports:
-        a,b=report.split()
-        if b not in users[a]["reporting_users"]:
-            users[a]["reporting_users"].add(b)
-            users[b]["reported_cnt"]+=1
+        u,v=report.split()
+        if v not in edges[u]:
+            edges[u].add(v)
+            indegree[v]+=1
     
+    black_list=set([node for node in indegree if indegree[node]>=k])
     
+    answer=[]
+    for node in id_list:
+        mail_cnt=0
+        for next_node in edges[node]:
+            if next_node in black_list:
+                mail_cnt+=1
+        answer.append(mail_cnt)
     
-    reported_users=set() 
-    for u in users:
-        if users[u]["reported_cnt"]>=k:
-            reported_users.add(u)
-    
-    
-    print("reported_users",reported_users)
-    answer = []
-    for u in users:
-        recevied_mail_cnt=0
-        for v in users[u]["reporting_users"]:
-            if v in reported_users:
-                recevied_mail_cnt+=1
-        answer.append(recevied_mail_cnt)
-    
+
     return answer
