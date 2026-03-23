a84d2cea574e92acbc40096f12b4fd483ffb771e
김동환 <66872094+forwarder1121@users.noreply.github.com>
2026-03-23 13:21:46 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 783.11 ms, Memory: 134 MB -BaekjoonHub

--- 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.py
+++ 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.py
@@ -1,30 +1,31 @@
 def solution(board, skill):
+    
     N=len(board)
     M=len(board[0])
-    
     diff=[[0]*(M+1) for _ in range(N+1)]
-    
-    for t,r1,c1,r2,c2,degree in skill:
-        if t==1:
+        
+    for s_type, r1,c1,r2,c2,degree in skill:
+        if s_type==1:
             degree*=-1
         diff[r1][c1]+=degree
         diff[r1][c2+1]-=degree
         diff[r2+1][c1]-=degree
         diff[r2+1][c2+1]+=degree
     
-    # horizental
-    for x in range(N+1):
-        for y in range(1,M+1):
+    # sum horizantal
+    for x in range(N):
+        for y in range(1,M):
             diff[x][y]+=diff[x][y-1]
-            
-    # vertical
-    for y in range(M+1):
-        for x in range(1,N+1):
+
+    # sum virtical
+    for y in range(M):
+        for x in range(1,N):
             diff[x][y]+=diff[x-1][y]
-     
+    
     answer = 0
     for x in range(N):
         for y in range(M):
-            if board[x][y]+diff[x][y]>=1:
+            if board[x][y]+diff[x][y]>0:
                 answer+=1
+    
     return answer
