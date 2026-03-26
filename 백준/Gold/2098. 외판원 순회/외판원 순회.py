0bb80bf5fa076ed3c591900a6b13f171a37596dd
김동환 <66872094+forwarder1121@users.noreply.github.com>
2026-03-26 16:10:17 +0900

    [Gold I] Title: 외판원 순회, Time: 1068 ms, Memory: 77640 KB -BaekjoonHub

--- 백준/Gold/2098. 외판원 순회/외판원 순회.py
+++ 백준/Gold/2098. 외판원 순회/외판원 순회.py
@@ -1,29 +1,31 @@
 import sys,math
-input = sys.stdin.readline
+from functools import lru_cache
+input=sys.stdin.readline
 
-N = int(input())
-W = [list(map(int, input().split())) for _ in range(N)]
+N=int(input())
+W=[list(map(int,input().split())) for _ in range(N)]
 
-INF = math.inf
-dp = [[INF] * (1 << N) for _ in range(N)]
+INF=math.inf
 
-dp[0][1] = 0
-
-for mask in range(1 << N):
-    for cur in range(N):
-        if dp[cur][mask] == INF:
+start=0
+@lru_cache(None)
+def P(cur,visit):
+    ''' Returns minimum cost to end circular path '''
+    # base
+    if visit==(1<<N)-1:
+        if W[cur][start]!=0:
+            return W[cur][start]
+        return INF
+    
+    best=INF
+    
+    for nxt in range(N):
+        if W[cur][nxt]==0:
             continue
-        for nxt in range(N):
-            if mask & (1 << nxt) or W[cur][nxt] == 0:
-                continue
-            dp[nxt][mask | (1 << nxt)] = min(
-                dp[nxt][mask | (1 << nxt)],
-                dp[cur][mask] + W[cur][nxt]
-            )
+        if visit&(1<<nxt):
+            continue
+        best=min(best,P(nxt,visit|(1<<nxt))+W[cur][nxt])
+    return best
 
-res = INF
-for cur in range(N):
-    if W[cur][0]:
-        res = min(res, dp[cur][(1 << N) - 1] + W[cur][0])
-
-print(res)
+answer=P(start,1<<start)
+print(answer)
