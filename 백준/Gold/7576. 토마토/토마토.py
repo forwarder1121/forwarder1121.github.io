c3177a5b262fe9f85f88e0a113b2b16bd344a612
김동환 <66872094+forwarder1121@users.noreply.github.com>
2026-04-04 19:54:44 +0900

    [Gold V] Title: 토마토, Time: 2684 ms, Memory: 108272 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -3,30 +3,33 @@ from collections import deque
 input=sys.stdin.readline
 
 M,N=map(int,input().split())
-boxes=[list(map(int,input().split())) for _ in range(N)]
-
-dx=[-1,1,0,0]
-dy=[0,0,-1,1]
+board=[list(map(int,input().split())) for _ in range(N)]
+visited=[[False]*M for _ in range(N)]
 
 queue=deque()
-for i in range(N):
-    for j in range(M):
-        if boxes[i][j]==1:
-            queue.append((i,j))
+remain=0
+for x in range(N):
+    for y in range(M):
+        if board[x][y]==1:
+            queue.append((x,y,0))
+        if board[x][y]==0:
+            remain+=1
 
+
+max_dist=0
 while queue:
-    cx,cy=queue.popleft()
-    for i in range(4):
-        nx=cx+dx[i]
-        ny=cy+dy[i]
+    cx,cy,cdist=queue.popleft()
+    max_dist=max(max_dist,cdist)
+    for dx,dy in [(-1,0),(1,0),(0,-1),(0,1)]:
+        nx,ny=cx+dx,cy+dy
         if 0<=nx<N and 0<=ny<M:
-            if boxes[nx][ny]==0:
-                boxes[nx][ny]=boxes[cx][cy]+1
-                queue.append((nx,ny))
+            if board[nx][ny]==0:
+                if not visited[nx][ny]:
+                    queue.append((nx,ny,cdist+1))
+                    visited[nx][ny]=True
+                    remain-=1
 
-for i in range(N):
-    for j in range(M):
-        if boxes[i][j]==0:
-            print(-1)
-            sys.exit()
-print(max(map(max,boxes))-1)
+if remain:
+    print(-1)
+else:
+    print(max_dist)
