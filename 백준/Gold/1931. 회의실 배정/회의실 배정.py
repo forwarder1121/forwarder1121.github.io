e2d2f769230fb273e95e5bbfe42c62d5bad91869
김동환 <66872094+forwarder1121@users.noreply.github.com>
2026-04-09 18:03:40 +0900

    [Gold V] Title: 회의실 배정, Time: 324 ms, Memory: 55508 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.py
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.py
@@ -1,25 +1,20 @@
-import sys
+import sys,heapq
 input=sys.stdin.readline
 
 N=int(input())
-meetings=[list(map(int,input().split())) for _ in range(N)]
+lines=[tuple(map(int,input().split())) for _ in range(N)]
 
-meetings.sort(key=lambda x:(x[1],x[0]))
+pq=[]
+for s,e in lines:
+    heapq.heappush(pq,(e,s))
 
 cur_time=0
-idx=0
 answer=0
-while True:
-    candidates=None
-    for i in range(idx,N):
-        if meetings[i][0]>=cur_time:
-            candidates=meetings[i]
-            idx=i+1
-            break
-    if not candidates:
-        break
-    
-    cur_time=candidates[1]
+while pq:
+    e,s=heapq.heappop(pq)
+    if s<cur_time:
+        continue
+    cur_time=e
     answer+=1
 
 print(answer)
