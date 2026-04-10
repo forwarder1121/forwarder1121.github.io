5a2a1730b095e39b7c92b47c10222a383207e304
김동환 <66872094+forwarder1121@users.noreply.github.com>
2026-04-10 10:02:46 +0900

    [Gold II] Title: 컵라면, Time: 532 ms, Memory: 64576 KB -BaekjoonHub

---
+++ 백준/Gold/1781. 컵라면/컵라면.py
@@ -0,0 +1,16 @@
+import sys,heapq
+input=sys.stdin.readline
+
+N=int(input())
+tasks=[tuple(map(int,input().split())) for _ in range(N)]
+
+tasks.sort()
+
+pq=[]
+for d,w in tasks:
+    heapq.heappush(pq,w)
+    if len(pq)>d:
+        heapq.heappop(pq)
+
+answer=sum(pq)
+print(answer)    
