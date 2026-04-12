36369f5d9c602c736305e459da1d0aef0983d5b6
김동환 <66872094+forwarder1121@users.noreply.github.com>
2026-04-12 16:23:37 +0900

    [Gold III] Title: LCS 2, Time: 432 ms, Memory: 57628 KB -BaekjoonHub

---
+++ 백준/Gold/9252. LCS 2/LCS 2.py
@@ -0,0 +1,36 @@
+# Time Complexity : O(NM)
+# N,M is length of each sequence
+import sys
+input=sys.stdin.readline
+
+A=input().strip()
+B=input().strip()
+
+N=len(A)
+M=len(B)
+dp=[[0]*(M+1) for _ in range(N+1)]
+
+for i in range(1,N+1):
+    for j in range(1,M+1):
+        if A[i-1]==B[j-1]:
+            dp[i][j]=dp[i-1][j-1]+1
+        else:
+            dp[i][j]=max(dp[i-1][j],dp[i][j-1])
+
+result=""
+i=N
+j=M
+while i>0 and j>0:
+    if A[i-1]==B[j-1]:
+        result+=A[i-1]
+        i-=1
+        j-=1
+    else:
+        if dp[i-1][j]>dp[i][j-1]:
+            i-=1
+        else:
+            j-=1
+
+answer=result[::-1]
+print(dp[N][M])
+print(answer)
